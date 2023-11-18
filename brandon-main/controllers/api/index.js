f1c63a39626c76e5d7a344bf831e9327b5895fed
brandonom <143345347+brandonom@users.noreply.github.com>
2023-11-18 16:49:36 -0500

    Delete brandon-main directory

--- brandon-main/controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./user_routes')
-const postRoutes = require('./post_routes')
-
-router.use('/user', userRoutes)
-router.use('/post', postRoutes)
-
-
-module.exports = router;
--- brandon-main/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const api =  require('./api')
-const viewRoutes =  require('./view_routes')
-
-
-router.use('/api', api)
-router.use('/', viewRoutes)
-
-module.exports = router;
